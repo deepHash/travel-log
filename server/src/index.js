b8d15bddd1e1502ef192249d4fbfcc5bb6100437
unknown <itsesisx@gmail.com>
2020-02-19 13:16:40 +0200

    added middlewares and errors handlers

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,26 @@
 const express = require('express'),
-      morgan = require('morgan'),
-      helmet = require('helmet'),
-            
+      morgan  = require('morgan'),
+      helmet  = require('helmet'),
+      cors    = require('cors'),
+
+      middlewares = require('./middlewares'),
+
       app = express(),
       port = process.env.PORT || 1337;
 
 app.use(morgan('common'));
+app.use(helmet());
+app.use(cors());
 
+
+app.get('/', (req,res) => {
+    res.json({
+        message: 'Hello World!'
+    });
+});
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
 app.listen(port, () => {
     console.log(`listening on http://localhost:${port}`);
